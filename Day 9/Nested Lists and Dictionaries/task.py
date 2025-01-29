f1ce7d825b05e82b04e53f12d6e182f3b99ce432
Tom <thomascoote89@gmail.com>
2025-01-29 17:22:02 +0000

    Worked on Day 9

--- Day 9/Nested Lists and Dictionaries/task.py
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -1,2 +1,21 @@
-#This is a test again
-#Editted on web
+from operator import index
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nested List in Dictionary
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+
+print(travel_log["Germany"]["cities_visited"][2])
