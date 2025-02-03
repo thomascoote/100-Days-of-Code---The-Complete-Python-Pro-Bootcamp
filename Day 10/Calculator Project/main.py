b60e3fd622067db66db60330628bf62182226c95
thomascoote <77835959+thomascoote@users.noreply.github.com>
2025-02-03 23:06:12 +0000

    Final day 10 commit

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -3,7 +3,7 @@ import art
 print(art.logo)
 
 def add(n1, n2):
-    return n1 + n2
+    return (n1) + (n2)
 
 def subtract(n1, n2):
     return n1 - n2
@@ -15,13 +15,31 @@ def divide(n1, n2):
     return n1/n2
 
 
-dict1 = {
-    '+': "add",
-    '-': "subtract",
-    '*': "multiply",
-    '/': "divide"
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
 }
 
-print(dict1['+'])
+counter = 0
+keep_going = "n"
+while counter == 0:
 
+    if keep_going == "n":
+        n1 = int(input("First number:"))
+    elif keep_going == "y":
+        n1 = answer
+    else:
+        exit()
+    n2 = int(input("Second number:"))
 
+    print("Choose an operation:")
+    for i in operations:
+        print(i)
+
+    operation = input("Choose an operation")
+
+    answer = operations[operation](n1,n2)
+    print(answer)
+    keep_going = input(f"Press Y to continue calculating with {answer} or N to start over:\n").lower()
