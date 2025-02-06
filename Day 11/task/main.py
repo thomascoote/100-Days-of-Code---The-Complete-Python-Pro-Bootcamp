fa8c8604ec2453b06b207ec60d3632198c2b99b3
thomascoote <77835959+thomascoote@users.noreply.github.com>
2025-02-06 08:36:45 +0000

    Final day 11 commit

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -27,31 +27,43 @@ print("Welcome to Blackjack!")
 
 your_cards = []
 cpu_cards = []
-your_aces_held = []
-cpu_aces_held = []
+your_aces_held = 0
+cpu_aces_held = 0
 
 def choose_random_card(who):
+    player_aces = 0
+    cpu_aces = 0
     chosen_card = random.choice(list(card_dict.keys()))
     print(f"{who} drew {chosen_card}")
+    if chosen_card == "Ace" and who == "player":
+        player_aces += 1
+    if chosen_card == "Ace" and who == "CPU":
+        cpu_aces += 1
     time.sleep(1)
-    return chosen_card
+    return chosen_card, player_aces, cpu_aces
 
 def draw_player():
-    your_cards.append(choose_random_card("You"))
+    your_cards.append(choose_random_card("player")[0])
+    player_aces = choose_random_card("player")[1]
 
 def draw_cpu():
-    cpu_cards.append(choose_random_card("CPU"))
-
+    cpu_cards.append(choose_random_card("CPU")[0])
+    cpu_aces = choose_random_card("CPU")[1]
 def current_score(n1):
     if n1 == "player":
         your_score = 0
         for i in your_cards:
             your_score += card_dict[i]
+        if your_aces_held > 0 and your_score > 21:
+            your_score -= 10
         return your_score
     else:
         cpu_score = 0
         for i in cpu_cards:
             cpu_score += card_dict[i]
+        if cpu_aces_held > 0 and cpu_score > 21:
+            cpu_score -= 10
+
         return cpu_score
 
 # Draw the initial two cards for the player and the CPU
@@ -69,7 +81,11 @@ while draw_again == "y":
     draw_player()
 
     print(current_score("player"))
+    if current_score("player") > 21:
+        print("Bust!")
+        exit()
+
     draw_again = input("Would you like to draw another card? Y/N\n").lower()
 
 print(f"Your cards:{your_cards}")
-print(f"Your cards:{cpu_cards}")
+print(f"CPU cards:{cpu_cards}")
