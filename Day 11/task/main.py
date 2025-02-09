f8c4e4199b7265f9c00e9fb3527eb77c24f4cbe0
tom <thomascoote89@gmail.com>
2025-02-09 23:00:12 +0000

    Blackjack Working

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,7 +1,6 @@
 import collections.abc
 
 import art
-import time
 import random
 
 card_dict = {
@@ -26,7 +25,8 @@ your_cards = []
 cpu_cards = []
 your_score = 0
 cpu_score = 0
-
+your_aces = 0
+cpu_aces = 0
 # Draw the initial two cards for the player and the CPU
 
 def draw(who):
@@ -63,10 +63,46 @@ cpu_draw_again = "y"
 
 player_draw_again = input("Would you like to draw another card? y/n\n").lower()
 
-print(player_draw_again)
-
 while player_draw_again == "y":
+    your_aces = 0
     your_cards.append(draw("You"))
     print(your_cards)
     update_scores(your_cards, cpu_cards)
+    your_score = update_scores(your_cards,cpu_cards)[0]
+    for i in your_cards:
+        if i == "Ace":
+            your_aces += 1
+
+    if your_score > 21:
+        if your_aces == 1:
+            your_score -= 10
+            if your_score > 21:
+                if your_aces == 2:
+                    your_score -= 10
+
+    if your_score > 21:
+        print(f"Your score = {your_score} \nBUST")
+        exit()
+
+    print(f"Your score = {your_score}\n")
+
+    cpu_score = update_scores(your_cards, cpu_cards)[1]
+    for i in cpu_cards:
+        if i == "Ace":
+            cpu_aces += 1
+
+    if cpu_score > 21:
+        if cpu_aces == 1:
+            cpu_score -= 10
+            if cpu_score > 21:
+                if cpu_aces == 2:
+                    cpu_score -= 10
+
+    if cpu_score < 17:
+        cpu_cards.append(draw("cpu"))
+        cpu_score = update_scores(your_cards, cpu_cards)[1]
+    print(cpu_cards)
+    print(f"CPU score = {cpu_score}")
+
     player_draw_again = input("Would you like to draw another card? y/n\n").lower()
+
