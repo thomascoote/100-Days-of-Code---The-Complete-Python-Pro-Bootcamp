eb623c16d224be5de53f030d12b5c709c85cc14b
Tom <thomascoote89@gmail.com>
2025-02-10 17:14:02 +0000

    Blackjack Branch

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,4 +1,5 @@
 import collections.abc
+import time
 
 import art
 import random
@@ -52,12 +53,33 @@ initial_draw()
 draw_again = input("Would you like to draw another card? y/n?").lower()
 while draw_again == "y":
     your_score = 0
-    cpu_score = 0
     your_cards.append(random.choice(list(card_dict.keys())))
     for i in your_cards:
         your_score += card_dict[i]
     print("Your Hand")
     print(your_cards)
+    time.sleep(1)
     print("Your Score")
     print(your_score)
+    time.sleep(1)
     draw_again = input("Would you like to draw again? y/n").lower()
+
+
+
+
+cpu_score = 0
+for i in cpu_cards:
+    cpu_score += card_dict[i]
+
+while cpu_score < 17:
+    new_cpu_card = []
+    new_cpu_card.append(random.choice(list(card_dict.keys())))
+    for i in new_cpu_card:
+        cpu_score += card_dict[i]
+
+    cpu_cards.append(new_cpu_card[0])
+    print(f"CPU Hand = {cpu_cards}")
+    print(f"CPU Score = {cpu_score}")
+    time.sleep(1)
+    print(cpu_score)
+
