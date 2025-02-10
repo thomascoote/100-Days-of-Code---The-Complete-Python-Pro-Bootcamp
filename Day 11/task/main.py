7d9ad122cb6f61d8bf7598376d57ab7ffee2d5ad
tom <thomascoote89@gmail.com>
2025-02-10 22:55:52 +0000

    Blackjack branch

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -48,6 +48,14 @@ def initial_draw():
     for i in cpu_cards:
         cpu_score += card_dict[i]
 
+    if len(your_cards) == 2 and your_score == 21:
+        print("Player Blackjack!")
+        exit()
+    if len(cpu_cards) == 2 and cpu_score == 21:
+        print("CPU Blackjack!")
+        exit()
+
+
 initial_draw()
 
 draw_again = input("Would you like to draw another card? y/n?").lower()
@@ -56,16 +64,29 @@ while draw_again == "y":
     your_cards.append(random.choice(list(card_dict.keys())))
     for i in your_cards:
         your_score += card_dict[i]
+
+
+    for i in your_cards:
+        if i == "Ace":
+            your_aces +=1
+
+    if your_score > 21 and your_aces == 1:
+        your_score -= 10
+    if your_score > 31 and your_aces == 2:
+        your_score -= 20
+
     print("Your Hand")
     print(your_cards)
     time.sleep(1)
     print("Your Score")
     print(your_score)
     time.sleep(1)
+
+    if your_score > 21:
+        print("Bust!")
+        exit()
+
     draw_again = input("Would you like to draw again? y/n").lower()
-
-
-
 
 cpu_score = 0
 for i in cpu_cards:
@@ -77,9 +98,45 @@ while cpu_score < 17:
     for i in new_cpu_card:
         cpu_score += card_dict[i]
 
+    for i in cpu_cards:
+        if i == "Ace":
+            your_aces +=1
+
+    if cpu_score > 21 and cpu_aces == 1:
+        cpu_score -= 10
+    if cpu_score > 31 and cpu_aces == 2:
+        cpu_score -= 20
+
     cpu_cards.append(new_cpu_card[0])
     print(f"CPU Hand = {cpu_cards}")
-    print(f"CPU Score = {cpu_score}")
     time.sleep(1)
-    print(cpu_score)
+    print(f"CPU Score = {cpu_score}")
 
+    if cpu_score > 21:
+        print("CPU Bust!")
+        time.sleep(1)
+        exit()
+
+# Calculate the final scores and decide who won
+
+your_final_score = 0
+cpu_final_score = 0
+for i in your_cards:
+    your_final_score += card_dict[i]
+for i in cpu_cards:
+    cpu_final_score += card_dict[i]
+
+if cpu_final_score < your_final_score < 22:
+    print(your_cards)
+    time.sleep(1)
+    print("Player Wins!")
+elif your_final_score < cpu_final_score < 22:
+    print(cpu_cards)
+    time.sleep(1)
+    print("CPU Wins!")
+else:
+    print("Draw!")
+
+time.sleep(1)
+
+exit()
